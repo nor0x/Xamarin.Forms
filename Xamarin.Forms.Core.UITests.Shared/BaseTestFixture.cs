49234987c861f1ab279f2459dab71c7997cc1347
Rui Marinho <me@ruimarinho.net>
2019-06-11 16:51:49 +0100

    Fix again

--- Xamarin.Forms.Core.UITests.Shared/BaseTestFixture.cs
+++ Xamarin.Forms.Core.UITests.Shared/BaseTestFixture.cs
@@ -55,13 +55,9 @@ namespace Xamarin.Forms.Core.UITests
 
 		protected abstract void NavigateToGallery();
 
-#pragma warning disable 618
-		[TestFixtureSetUp]
-#pragma warning restore 618
 		protected virtual void FixtureSetup()
 		{
 			LaunchApp();
-			ResetApp();
 
 			int maxAttempts = 2;
 			int attempts = 0;
